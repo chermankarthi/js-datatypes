002040a0407e640c205aaffe52bee3ec3176a811
chermankarthi <chermankarthick@gmail.com>
2023-07-26 21:49:28 +0530

    add

---
+++ main.js
@@ -0,0 +1,39 @@
+
+// string
+let name="Cherman";
+let profession=`Developer`;
+let locality='Tenkasi';
+console.log(name, profession, locality)
+
+
+
+
+// number
+let num1=20;
+let num2=2.5;
+console.log(num1, num2);
+
+
+// Bigint
+let lng1=12758859485948n;
+let lng2=7837849344389393783n;
+let result=lng1+lng2;
+console.log(result)
+
+// booleaan
+confirm();
+
+
+
+// undefined
+let name1;
+console.log(name1);
+
+// null
+let names=null;
+console.log(names);
+
+
+
+
+
